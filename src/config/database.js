8555f439d6378e24ee1eb6847cc37a8feaee5244
euricoleao <euricobig@gmail.com>
2024-02-27 23:47:09 -0300

    projeto code burger

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'codepostgres',
+    database: 'starburgeron',
+    define: {
+        timespamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+}
